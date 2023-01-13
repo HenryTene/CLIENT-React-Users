dd3226efad8d9ab7f497ed63827f1c37232f463b
Henry Tene <henry_tene@hotmail.com>
2023-01-13 12:11:03 -0500

    se crea la tabla para traer los registros de la base de datos de las pautas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Dashboard from "./components/Dashboard";
 import Nuevo from "./components/Nuevo";
 import Editar from "./components/Editar";
 import Home from "./components/Home";
+import Tabla from "./components/Tabla";
+import TablaPautas from "./components/TablaPautas";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
@@ -22,10 +24,14 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Nuevo />} />
+
           <Route element={<ProtectedRoute user={userResp} />}>
             <Route exact path="/user-profile/:id" element={<Editar />} />
             <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/usuarios" element={<Tabla />} />
+            <Route exact path="/pautas" element={<TablaPautas />} />
           </Route>
+          
         </Routes>
       </Router>
     </React.Fragment>
